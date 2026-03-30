084656a4f33b70236b01670f9fd6f8f87e59e05d
sophiajamiebasuncion-dot <sophiajamiebasuncion@gmail.com>
2026-03-30 21:56:40 +0400

    Add files via upload

---
+++ 7-Some Counting.py
@@ -0,0 +1,19 @@
+print("Counting up from 0-50 in increments of 1")
+for i in range(0, 51):
+    print(i)
+
+print("\nCounting down from 50-0 in decrements of 1")
+for i in range(50, -1, -1):
+    print(i)
+
+print("\nCounting up from 30-50 in increments of 1")
+for i in range(30, 51):
+    print(i)
+
+print("\nCounting down from 50-10 in decrements of 2")
+for i in range(50, 9, -2):
+    print(i)
+
+print("\nCounting up from 100-200 in increments of 5")
+for i in range(100, 201, 5):
+    print(i)
