e6b21570046978345d54d7cdf25c24d71bcc417e
sophiajamiebasuncion-dot <sophiajamiebasuncion@gmail.com>
2026-03-30 22:15:04 +0400

    Add files via upload

---
+++ 8-Simple Search.py
@@ -0,0 +1,8 @@
+names = ["Jake" "Zac", "Ian", "Ron", "Sam", "Dave"]
+
+search_name = "Sam"
+
+if search_name in names:
+    print(f"{search_name} was found")
+else:
+    print(f"{search_name} was not found")
